e2908eef7af6b55d96ac613034ddab0738ba5fb2
juaestiv <juaestiv@gmail.com>
2020-11-06 19:54:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 
 //In FC It must be declared with const or let
 const App = () => {
-
   // Initial state
   const state = {
     //state 1
@@ -17,17 +16,17 @@ const App = () => {
     //State 2
     otherState:"Some other state"
   }
-
+  
   // Assign the initial state to currentPeron and
   // creates the function to change the state that will force to 
   // re-render de component
   const [currentPerson, setPersonState] = useState(state) 
 
   // Convention. Ends in Handler
-  const clickMeHandler = () => {
+  const clickMeHandler = (newName) => {
     setPersonState ({
       persons: [
-        {name:"Juan Antonio", age:22},
+        {name:newName, age:22},
         {name:"Antoine", age:23},
         {name:"Anthony", age:24}
       ]
@@ -37,8 +36,9 @@ const App = () => {
   return (
     <div className="App">
       <h1>Testing Hooks</h1>
-      // Function call. Use curly braces and don't end with ()
-      <button onClick={clickMeHandler}>Click Me!</button>
+      {/* // Function call. Use curly braces and don't end with () */}
+      <button onClick={clickMeHandler.bind(this,'max')}>Click Me!</button>
+      <button onClick={() => clickMeHandler('Another mode')}>Click Me 2!</button>
 
         {/* hardcoded */}
         <Person name="Juan" age="22"/>
@@ -47,13 +47,15 @@ const App = () => {
 
         {/* Sending objects properties as parameters */}
         <Person name={currentPerson.persons[0].name} age={currentPerson.persons[0].age}/>
-        <Person name={currentPerson.persons[1].name} age={currentPerson.persons[1].age}/>
+        <Person
+        name={currentPerson.persons[1].name} 
+        age={currentPerson.persons[1].age}
+        funtionClick={clickMeHandler.bind(this,'juan')}/>
         <Person name={currentPerson.persons[2].name} age={currentPerson.persons[2].age}>Esta persona es especial</Person>
         
         {/* Using array features */}
         {currentPerson.persons.map((num)=>{console.log("test " + num.name)})}
         {currentPerson.persons.map((num)=>  <Person name={num.name} age={num.age}/>)}
-
 
     </div>
   );
--- src/Components/Person/Person.js
+++ src/Components/Person/Person.js
@@ -4,8 +4,9 @@ const person = (props) => {
 
     return (
         <div>
-            <h1>La persona es {props.name} y su edad es {props.age}</h1>
-            <p>{props.children}</p>
+            {/* //onClick can call to a function of App. If that function is sent in props */}
+            <h1 onClick={props.funtionClick}>La persona es {props.name} y su edad es {props.age}</h1>
+            <p >{props.children}</p>
         </div>
 
     );
